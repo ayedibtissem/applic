af2c0c786f7472dd433989b45f769b95e5568c78
ayedibtissem <116677898+ayedibtissem@users.noreply.github.com>
2023-03-31 00:37:21 +0100

    Update api.js

--- src/redux/api.js
+++ src/redux/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API = axios.create({ baseURL: "https://visitapp0.onrender.com/" });
+const API = axios.create({ baseURL: "https://visitappibty.onrender.com" });
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
     req.headers.Authorization = `Bearer ${
